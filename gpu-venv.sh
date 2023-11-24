73ecc56857aacbffc3eb05462dd18026017cc2b1
whitgroves <whitney.groves@gmail.com>
2023-11-24 16:31:22 -0600

    project 4 follow up 2/?

--- gpu-venv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 -m venv .gpu 
-source ./.gpu/bin/activate
-python3 -m pip install -U --extra-index-url=https://pypi.nvidia.com pip cudf-cu12 dask-cudf-cu12 tensorflow[and-cuda] scikit-learn ipykernel
